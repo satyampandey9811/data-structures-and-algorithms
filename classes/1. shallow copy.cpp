d6d8eb837bd73a4dd183131495e2f82f41e2e406
Satyam Pandey <satyampandey9811@gmail.com>
2020-03-10 22:10:32 +0530

    shallow copy

---
+++ classes/1. shallow copy.cpp
@@ -0,0 +1,33 @@
+// shallow copy in class
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class student{
+    int age;
+    char* name;
+public:
+    student(int age, char* name){
+        this -> age = age;
+        this -> name = name;
+    }
+    void display(){
+        cout << name << ' ' << age << endl;
+    }
+};
+
+int main(){
+    char name[] = "abcd";
+    student s1(20, name);
+    s1.display();
+
+    name[3] = 'e';
+    student s2(24, name);
+    s2.display();
+    s1.display();
+}
+
+// output is 
+abcd 20
+abce 24
+abce 20
