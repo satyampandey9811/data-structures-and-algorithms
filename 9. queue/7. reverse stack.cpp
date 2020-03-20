8afe819900a053deee9778b77bd7674bc3b3cac3
Satyam Pandey <satyampandey9811@gmail.com>
2020-03-20 19:53:47 +0530

    reverse stack

---
+++ 9. queue/7. reverse stack.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+#include <stack>
+
+void reverseStack(stack<int> &input, stack<int> &extra) {
+	
+    while(!input.empty()){
+        int val = input.top();
+        extra.push(val);
+        input.pop();
+    }
+    input = extra;
+}
+
+int main() {
+    stack<int> s1, s2;
+    int size;
+    cin >> size;
+
+    int val;
+    for(int i = 0; i < size; i++) {
+    	cin >> val;
+    	s1.push(val);
+    }
+    reverseStack(s1, s2);
+    while(!s1.empty()) {
+    	cout << s1.top() << " ";
+    	s1.pop();
+    }
+}
