b834a588353b38f483a940bc223e03b54f66d4ea
Satyam Pandey <satyampandey9811@gmail.com>
2020-03-27 18:02:52 +0530

    node with maximum child sum

---
+++ 11. tree/16. node with maximum child sum.cpp
@@ -0,0 +1,81 @@
+// tree
+// node with maximum child sum
+
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+class TreeNode {
+public:
+    T data;
+    vector<TreeNode<T>*> children;
+    
+    TreeNode(T data) {
+        this->data = data;
+    }
+    
+    ~TreeNode() {
+        for (int i = 0; i < children.size(); i++) {
+            delete children[i];
+        }
+    }
+    
+};
+
+TreeNode<int>* takeInputLevelWise() {
+    int rootData;
+    cin >> rootData;
+    TreeNode<int>* root = new TreeNode<int>(rootData);
+    
+    queue<TreeNode<int>*> pendingNodes;
+    
+    pendingNodes.push(root);
+    while (pendingNodes.size() != 0) {
+        TreeNode<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+        int numChild;
+        cin >> numChild;
+        for (int i = 0; i < numChild; i++) {
+            int childData;
+            cin >> childData;
+            TreeNode<int>* child = new TreeNode<int>(childData);
+            front->children.push_back(child);
+            pendingNodes.push(child);
+        }
+    }
+    return root;
+}
+
+TreeNode<int>* maxSumNode(TreeNode<int> *root){
+    if(root == NULL)
+        return NULL;
+    TreeNode<int>* ans;
+    queue<pair<TreeNode<int>*, int>> q;        
+    q.push({root, root -> data});
+    int sum, curr, res = 0;
+
+    while(q.size() != 0){
+        TreeNode<int>* front = q.front().first;
+        sum = 0;
+        q.pop();
+        for(int i = 0; i < front -> children.size(); i++){
+            curr = front -> children[i] -> data;
+            sum += curr;
+            q.push({front -> children[i], curr});            
+        }
+        sum += front -> data;
+        if(res < sum){
+            res = sum;
+            ans = front;
+        }        
+    }
+    return ans;
+}
+
+int main() {
+    TreeNode<int>* root = takeInputLevelWise();
+    TreeNode<int>* ans = maxSumNode(root);
+    if(ans != NULL) {
+        cout << ans -> data << endl;
+    }
+}
