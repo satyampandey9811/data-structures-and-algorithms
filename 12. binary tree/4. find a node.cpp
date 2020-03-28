a7a17eb107f83da0767ee76243899d23663faabe
Satyam Pandey <satyampandey9811@gmail.com>
2020-03-28 17:26:12 +0530

    find a node

---
+++ 12. binary tree/4. find a node.cpp
@@ -0,0 +1,73 @@
+// binary tree
+// find a node
+
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+class BinaryTreeNode {
+    public : 
+    T data;
+    BinaryTreeNode<T> *left;
+    BinaryTreeNode<T> *right;
+
+    BinaryTreeNode(T data) {
+        this -> data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+BinaryTreeNode<int>* takeInput() {
+    int rootData;
+    //cout << "Enter root data : ";
+    cin >> rootData;
+    if(rootData == -1) {
+        return NULL;
+    }
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int>*> q;
+    q.push(root);
+    while(!q.empty()) {
+        BinaryTreeNode<int> *currentNode = q.front();
+	q.pop();
+        int leftChild, rightChild;
+        //cout << "Enter left child of " << currentNode -> data << " : ";
+        cin >> leftChild;
+        if(leftChild != -1) {
+            BinaryTreeNode<int>* leftNode = new BinaryTreeNode<int>(leftChild);
+            currentNode -> left =leftNode;
+            q.push(leftNode);
+        }
+        //cout << "Enter right child of " << currentNode -> data << " : ";
+        cin >> rightChild;
+        if(rightChild != -1) {
+            BinaryTreeNode<int>* rightNode = new BinaryTreeNode<int>(rightChild);
+            currentNode -> right =rightNode;
+            q.push(rightNode);
+        }
+    }
+    return root;
+}
+
+bool isNodePresent(BinaryTreeNode<int>* root, int x) {    
+    if(root == NULL) return false;
+    if(root -> data == x) return true;
+    bool ans = isNodePresent(root -> left, x);
+    if(ans) return true;
+    ans = isNodePresent(root -> right, x);
+    return ans;
+}
+
+int main() {
+    BinaryTreeNode<int>* root = takeInput();
+    int x;
+    cin >> x;
+    if(isNodePresent(root, x)) {
+        cout << "true";
+    }
+    else {
+        cout << "false";
+    }
+    
+}
