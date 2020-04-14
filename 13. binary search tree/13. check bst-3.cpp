64c39a0c9328306890175ee0ff9be94471c80f58
Satyam Pandey <satyampandey9811@gmail.com>
2020-04-14 23:42:11 +0530

    check bst-3

---
+++ 13. binary search tree/13. check bst-3.cpp
@@ -0,0 +1,75 @@
+// binary search tree
+// 13. check bst-3
+
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+class BinaryTreeNode {
+    public : 
+    T data;
+    BinaryTreeNode<T> *left;
+    BinaryTreeNode<T> *right;
+
+    BinaryTreeNode(T data) {
+        this -> data = data;
+        left = NULL;
+        right = NULL;
+    }
+    ~BinaryTreeNode() {
+    	if(left) 
+       		delete left;
+    	if(right) 
+       		delete right;
+    }
+};
+
+BinaryTreeNode<int>* takeInput() {
+    int rootData;
+    //cout << "Enter root data : ";
+    cin >> rootData;
+    if(rootData == -1) {
+        return NULL;
+    }
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int>*> q;
+    q.push(root);
+    while(!q.empty()) {
+        BinaryTreeNode<int> *currentNode = q.front();
+	q.pop();
+        int leftChild, rightChild;
+        //cout << "Enter left child of " << currentNode -> data << " : ";
+        cin >> leftChild;
+        if(leftChild != -1) {
+            BinaryTreeNode<int>* leftNode = new BinaryTreeNode<int>(leftChild);
+            currentNode -> left =leftNode;
+            q.push(leftNode);
+        }
+        //cout << "Enter right child of " << currentNode -> data << " : ";
+        cin >> rightChild;
+        if(rightChild != -1) {
+            BinaryTreeNode<int>* rightNode = new BinaryTreeNode<int>(rightChild);
+            currentNode -> right =rightNode;
+            q.push(rightNode);
+        }
+    }
+    return root;
+}
+
+bool isBST(BinaryTreeNode<int> *root, int minval = INT_MIN, int maxval = INT_MAX){
+	if(root == NULL)
+        return true;
+    
+    return (root -> data > minval) and (root -> data <= maxval) and isBST(root -> left, minval, root -> data) and isBST(root -> right, root -> data, maxval);
+}
+
+int main() {
+    BinaryTreeNode<int>* root = takeInput();
+    if(isBST(root)) {
+        cout << "true" << endl;
+    }
+    else {
+        cout << "false" << endl;
+    }
+    delete root;
+}
