943e4130bb7bd494c0428a66415289af84c2b566
Satyam Pandey <satyampandey9811@gmail.com>
2020-04-27 00:16:37 +0530

    Min priority queue implementation

---
+++ 15. priority queue/1. Min priority queue implementation.cpp
@@ -0,0 +1,67 @@
+// priority queue
+// implementation of Min priority queue
+
+class priorityQueue {
+	vector<int> pq;
+	//int nextIndex;
+	
+public:
+	
+	priorityQueue() {
+		nextIndex = 0;
+	}
+	
+	bool isEmpty() {
+		return pq.size() == 0;
+	}
+	
+	int getSize() {
+		return pq.size();
+	}
+	
+	int getMin() {
+		if(isEmpty()) return 0;
+		return pq[0];
+	}
+	
+	void insert(int data) {
+		pq.push_back(data);
+		int cI = pq.size() - 1;
+		
+		while(cI > 0){
+			int pI = (cI - 1) / 2;
+			if(pq[cI] < pq[pI]) 
+				swap(pq[cI], pq[pI]);
+			else 
+				break;
+			cI = pI;
+		}
+	}
+	
+	int removeMin() {
+		if(isEmpty()) return 0;
+        int ans = pq[0];
+		pq[0] = pq[pq.size() - 1];		
+		pq.pop_back();
+		int pI = 0;
+		int lC = pI * 2 + 1, rC = pI * 2 + 2;
+		
+		while(lC < pq.size() - 1) {
+            if(pq[pI] > min(pq[lC], pq[rC])) {
+                if(pq[lC] > pq[rC]) {
+                    swap(pq[pI], pq[rC]);
+                    pI = rC;
+                }
+                else {
+                    swap(pq[pI], pq[lC]);
+                    pI = lC;
+                }
+            }
+            else
+                break;
+			lC = 2 * pI + 1;
+			rC = 2 * pI + 2;
+		}
+		return ans;
+	}
+};
